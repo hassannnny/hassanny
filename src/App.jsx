bc226c852e5421d923c6180b8093e8e711449f85
hassannnny <lhjaved@neiu.edu>
2025-09-10 12:27:36 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Container, Row, Col, Nav, NavLink, NavbarBrand } from 'reactstrap'
+import miLogo from './assets/miLogo.png'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <Container>
+
+      <Row style={{height: '10vh', width: '100vw'}}>
+        <Nav style={{color: 'white', margin: 0, paddingTop: '2vh'}}>
+          <NavbarBrand style={{paddingRight: '10vw', paddingLeft: '5vw', fontWeight: 'bolder', fontSize: 'large'}}>portfolio</NavbarBrand>
+          <NavLink style={{paddingRight: '5vw'}}>Resume</NavLink>
+          <NavLink style={{paddingRight: '5vw'}}>Work</NavLink>
+          <NavLink style={{paddingRight: '5vw'}}>Contact</NavLink>
+        </Nav>
+      </Row>
+      
+
+
+
+      <Row style={{display: 'inline-flex', height: '90vh', width: '100vw'}}>
+        <Col style={{height: '100%', width: '50vw', textAlign: 'center', alignContent: 'center', color: 'white'}}>
+          <h1>Liaquat</h1>
+          <h1>Hassan</h1>
+          <h1>Javed</h1>
+        </Col>
+        <Col style={{height: '100%', width: '50vw', textAlign: 'center', alignContent: 'center'}}>
+          <img src={miLogo} height={400} />
+        </Col>
+      </Row>
+    </Container>
+    
   )
 }
 
